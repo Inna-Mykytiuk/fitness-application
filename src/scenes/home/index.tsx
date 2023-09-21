56f4e9b03af9e2f0a6f467b0f96059adf8a119c2
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-09-21 14:39:44 +0300

    add moyion to main hero title

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -7,6 +7,7 @@ import HomePageGraphic from "../../assets/HomePageGraphic.png";
 import SponsorRedBull from "../../assets/SponsorRedBull.png";
 import SponsorForbes from "../../assets/SponsorForbes.png";
 import SponsorFortune from "../../assets/SponsorFortune.png";
+import { motion } from "framer-motion";
 
 type Props = {
   children?: React.ReactNode;
@@ -20,44 +21,56 @@ const Home = ({ setSelectedPage }: Props) => {
   return (
     <section
       id="home"
-      className="gap-16 bg-gray20 py-10 md:h-full md:pb-0"
+      className="gap-16 bg-gray20 py-20 md:h-full md:pb-0"
     >
       <div className={`${flexBetween} mx-auto w-5/6`}>
         {/* IMAGE AND MAIN HEADER */}
         <div className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6">
           {/* MAIN HEADER */}
           <div className="z-10 mt-32 md:basis-3/5">
+
             {/* HEADINGS */}
-            <div className="md:-mt-20">
-              <div className="">
-                <div>
-                  <img alt="home-page-taxr" src={HomePageText}></img>
+            <motion.div 
+            className="md:-mt-20"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{once: true, amount: 0.5}}
+            transition={{duration: 0.5}}
+            variants={{
+              hidden: {opacity:0, x:-50},
+              visible: {opacity: 1, x:0},
+            }}
+            >
+              <div className="relative">
+                <div className="before:absolute before:-top-20 md:before:content-evolvetext before:-left-20 before:z-[-1]">
+                  <img alt="home-page-text" src={HomePageText}></img>
                   {/* <p className="text-[86px] text-rose800 font-black font-family" >Mini Forest</p>
               <p className="text-[42px] text-rose800 font-thin tracking-[5px]" >Evolutionary fitness.</p> */}
                 </div>
               </div>
 
-              <p className="mt-8 font-montserrat text-md ">
+              <p className="mt-8 font-montserrat sm:text-sm text-md">
                 Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
                 Studios to get the Body Shapes That you Dream of.. Get Your Dream
                 Body Now.
               </p>
-            </div>
+            </motion.div>
 
             {/* ACTIONS */}
-            <div>
+            <div className="mt-8 flex items-center gap-8">
               <ActionButton setSelectedPage={setSelectedPage}>Join now</ActionButton>
-            </div>
-            <AnchorLink className="text-sm font-bold text-primary500 underline hover:text-secondary500"
+              <AnchorLink className="text-sm font-bold text-primary500 underline hover:text-secondary500"
               onClick={() => setSelectedPage(SelectedPage.ContactUs)}
               href={`#${SelectedPage.ContactUs}`}>
               <p>Learn More</p>
 
             </AnchorLink>
+            </div>
+            
           </div>
 
           {/* IMAGE */}
-          <div>
+          <div className="flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-end">
             <img alt="home-page-graphic" src={HomePageGraphic}></img>
           </div>
         </div>
